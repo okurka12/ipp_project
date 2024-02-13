00f03a4493903a853cf13083ca5eae37d645a34c
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-13 06:14:46 +0100

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-echo "python3.10 ./parse.py $@"
+#!/bin/bash
+# this script runs the parse.py script using the correct version of python
+# usage: ./run.sh [ARGUMENTS]
+
+# echo "python3.10 ./parse.py $@"
 python3.10 ./parse.py $@
